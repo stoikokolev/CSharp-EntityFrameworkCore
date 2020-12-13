0ee29c2cda64b65e0b07172b197a2b8fbcceb790
Stoyko Kolev <stoikokolev@gmail.com>
2020-12-13 16:56:35 +0200

    connection string to DataSettings class

---
+++ SoftUni/Data/DataSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace SoftUni.Data
+{
+    public class DataSettings
+    {
+        public const string DefaultConnection =
+            @"Server=STOYKO-PC\SQLEXPRESS;Database=CarSystem;Integrated Security=True;";
+    }
+}
--- SoftUni/Data/SoftUniContext.cs
+++ SoftUni/Data/SoftUniContext.cs
@@ -30,8 +30,8 @@ namespace SoftUni.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=STOYKO-PC\\SQLEXPRESS;Database=SoftUni;Integrated Security=True;");
+                optionsBuilder
+                    .UseSqlServer(DataSettings.DefaultConnection);
             }
         }
 
