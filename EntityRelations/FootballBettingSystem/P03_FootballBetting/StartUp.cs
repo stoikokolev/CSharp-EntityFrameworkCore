2595504329be3d64dcc228a07acb7811bbf7e6b3
Stoyko Kolev <stoikokolev@gmail.com>
2020-12-13 20:28:36 +0200

    ready for judge

--- EntityRelations/FootballBettingSystem/P03_FootballBetting/StartUp.cs
+++ EntityRelations/FootballBettingSystem/P03_FootballBetting/StartUp.cs
@@ -1,10 +1,29 @@
-﻿namespace P03_FootballBetting
+﻿using System;
+using System.Linq;
+using P03_FootballBetting.Data;
+
+namespace P03_FootballBetting
 {
-   public class StartUp
+    public class StartUp
     {
         public static void Main()
         {
-            
+            var context = new FootballBettingContext();
+
+            var users = context
+                .Users
+                .Select(u => new
+                {
+                    u.UserName,
+                    u.Email,
+                    Name = u.Name == null ? "(No name)" : u.Name
+                });
+
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.UserName} -> {user.Email} Name: {user.Name}");
+            }
+
         }
     }
 }
