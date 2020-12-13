87954bbb4fa5ef1e887898f19e3bad3999ba6fc6
Stoyko Kolev <stoikokolev@gmail.com>
2020-12-13 19:01:11 +0200

    OnModelCreating filled with the entities

---
+++ EntityRelations/FootballBettingSystem/P03_FootballBetting.Data/DataSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace P03_FootballBetting.Data
+{
+    internal static class DataSettings
+    {
+        internal static string DefaultConnection =
+            "Server=STOYKO-PC\\SQLEXPRESS;Database=FootballBettingSystem;Integrated Security = True";
+    }
+}
