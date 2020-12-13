730f06c8b332f899d14e279542d19750a78acf2d
Stoyko Kolev <stoikokolev@gmail.com>
2020-12-13 16:57:10 +0200

    projects architecture

---
+++ EntityRelations/FootballBettingSystem/P03_FootballBetting.Data.Models/Team.cs
@@ -0,0 +1,23 @@
+﻿namespace P03_FootballBetting.Data.Models
+{
+    public class Team
+    {
+        public int TeamID { get; set; }
+
+        public string Name { get; set; }
+
+        public string LogoUrl { get; set; }
+
+        public string Initials { get; set; }
+
+        public decimal Budget { get; set; }
+
+        //TODO: Navigation properties
+        public int PrimaryKitColorId { get; set; }
+
+        //TODO: Navigation properties
+        public int SecondaryKitColorId { get; set; }
+
+        public int TownId { get; set; }
+    }
+}
---
+++ EntityRelations/FootballBettingSystem/P03_FootballBetting.Data/FootballBettingContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P03_FootballBetting.Data
+{
+    public class FootballBettingContext:DbContext
+    {
+        //TODO: Complete DbContext
+    }
+}
